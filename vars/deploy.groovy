daa547b03409531e32240c25054c1892053d2113
AMRUTHASALIKE <31771178+AMRUTHASALIKE@users.noreply.github.com>
2018-12-20 12:21:14 +0530

    Update deploy.groovy

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -20,7 +20,7 @@ def call(String key_id)
   {
        dir('SpringMVCSecurityXML/target') 
        {         
-         sshagent(['${key_id}']) 
+         sshagent(["${key_id}"]) 
          {
            //scp SpringMVCSecurityXML.war /var/lib/tomcat8/webapps
            sh "scp /home/ubuntu/f1 ubuntu@52.47.190.123:~"
