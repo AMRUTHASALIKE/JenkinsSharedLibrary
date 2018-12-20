fee7b9c9f534cd88550c9cce8019c030d8665fea
AMRUTHASALIKE <31771178+AMRUTHASALIKE@users.noreply.github.com>
2018-12-20 13:02:33 +0530

    Update deploy.groovy

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -14,9 +14,9 @@ def call()
 }
 */
 
-def call(String key_id)
+def call(String key_id, String user, String ip)
 {
-  echo "${key_id}"
+  //echo "${key_id}"
   stage('Deploy')
   {
        dir('SpringMVCSecurityXML/target') 
@@ -26,7 +26,7 @@ def call(String key_id)
          {
            //scp SpringMVCSecurityXML.war /var/lib/tomcat8/webapps
            sh "echo ${key_id}"
-           sh "scp /home/ubuntu/f1 ubuntu@52.47.190.123:~"
+           sh "scp /home/ubuntu/f1 ${user}@${ip}:~"
          }
 
        }
