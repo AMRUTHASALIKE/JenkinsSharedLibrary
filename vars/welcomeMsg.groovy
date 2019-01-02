9dd412f9b81b3d53f62426a2450501ab35aa1efa
AMRUTHASALIKE <31771178+AMRUTHASALIKE@users.noreply.github.com>
2019-01-02 12:21:59 +0530

    Update welcomeMsg.groovy

--- vars/welcomeMsg.groovy
+++ vars/welcomeMsg.groovy
@@ -1,7 +1,11 @@
 #!/usr/bin/env groovy
 
+import hudson.slaves.EnvironmentVariablesNodeProperty
+import hudson.EnvVars
+
+
 def call(String name = 'amrutha') {
   echo "Hello, ${name}."
   echo env.deploy_user
   echo env['deploy_user']
-}
+  echo "${env.deploy_user}"
